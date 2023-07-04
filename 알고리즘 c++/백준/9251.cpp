208943d0502db9bc28599827e22ac11ab5fee60e
ParkRaechan <focks1012@naver.com>
2023-07-04 17:17:27 +0900

    백준

--- 알고리즘 c++/백준/9251.cpp
+++ 알고리즘 c++/백준/9251.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int arr[27];
+int dp[1001][1001];
 int main(int argc,const char *argv[]){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string s1,s2; int index=0;
-	cin>>s1>>s2;
-	for(int i=0;i<s1.length();i++){
-		arr[s1[i]-'A']++;
-	}
-	for(int i=0;i<s2.length();i++){
-		if(arr[s2[i]-'A']!=0){
-			index++; arr[s2[i]-'A']--;
+	string a,b;	cin>>a>>b;
+	int result=0;
+	for(int i=1;i<=a.length();i++){
+		for(int j=1;j<=b.length();j++){
+			if(a[i-1]==b[j-1]){
+				dp[i][j]=dp[i-1][j-1]+1;
+			}else{
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+			}
 		}
 	}
-	cout<<index;
+	cout<<dp[a.length()][b.length()];
 	
 	return 0;
 }
