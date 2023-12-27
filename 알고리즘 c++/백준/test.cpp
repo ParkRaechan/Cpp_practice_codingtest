2c72c04a8436ebe79a192a958df765eb1496c36b
ParkRaechan <focks1012@naver.com>
2023-12-27 19:21:36 +0900

    merge revert test

--- 알고리즘 c++/백준/test.cpp
+++ 알고리즘 c++/백준/test.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
-int arr[500001];
+vector<pair<int,int>> v; 
+bool compare1(pair<int, int> a, pair<int, int> b) {
+	if(a.first==b.first) return a.second < b.second;
+	return a.first > b.first;
+}
+bool compare2(pair<int, int> a, pair<int, int> b) {
+	if(a.second==b.second) return a.first > b.first;
+	return a.second < b.second;
+}
 int main(int argc,const char *argv[]){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	int n,result=0;
+	int n;
+	//test
 	cin>>n;
 	for(int i=0;i<n;i++){
-		cin>>arr[i];
+		int a,b;
+		cin>>a>>b;
+		v.push_back({a,b});
 	}
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n-1-i;j++){
-			if(arr[j]>arr[j+1]){
-				int temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-				result++;
-			}
-		}
-	}
-	cout<<result;
+	sort(v.begin(),v.end(),compare1);
+	cout<<v[0].first<<" "<<v[0].second<<" "<<v[1].first<<" "<<v[1].second<<"\n";
+	sort(v.begin(),v.end(),compare2);
+	cout<<v[0].first<<" "<<v[0].second<<" "<<v[1].first<<" "<<v[1].second<<"\n";
 	
 	return 0;
 }
