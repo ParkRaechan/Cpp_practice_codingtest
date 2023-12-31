4bbbdc324a8e16273b4da7324ab04fcb0b17b692
ParkRaechan <focks1012@naver.com>
2023-12-31 15:45:50 +0900

    백준 및 우선순위 큐 클래스

---
+++ 알고리즘 c++/백준/11279.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+priority_queue<int> p_q;
+int main(int argc,const char *argv[]){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;	cin>>n;
+	for(int i=0;i<n;i++){
+		int m; cin>>m;
+		if(m==0){
+			if(!p_q.empty()){
+				cout<<p_q.top()<<"\n";
+				p_q.pop();
+			}else cout<<"0\n";
+		}else{
+			p_q.push(m);
+		}
+	}
+	
+	return 0;
+}
